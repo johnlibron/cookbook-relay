ec99777b93be0888852a9c6d847bc06073319f08
John James Libron <johnjames.libron@gmail.com>
2021-02-26 00:46:23 +0800

    Add graphql schema query: category, allCategories, ingredient, allIngredients

--- cookbook/settings.py
+++ cookbook/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cookbook.ingredients',
+    'graphene_django',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+GRAPHENE = {
+    'SCHEMA': 'cookbook.schema.schema'
+}
